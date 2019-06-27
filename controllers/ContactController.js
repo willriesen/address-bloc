e2affe1b3c555f0fc190ab66ea947b57daca07b4
Will Riesen <williamdriesen@Williams-MacBook-Pro.local>
2019-06-27 16:51:26 -0500

    checkpoint listing contacts complete

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -1,4 +1,3 @@
-const inquirer = require("inquirer");
 const Contact = require("../db/models").Contact;
 
 module.exports = class ContactController {
@@ -31,10 +30,87 @@ module.exports = class ContactController {
               }
             }
           ];
-      }
-   
+      
+        this.searchQuestions = [
+        {
+          type: "input",
+          name: "name",
+          message: "Name of contact to search - ",
+          validate(val){
+            return val !== "";
+          }
+        }
+      ];
+
+        this.showContactQuestions = [
+        {
+          type: "list",
+          name: "selected",
+          message: "Please choose from an option below: ",
+          choices: [
+            "Delete contact",
+            "Main menu"
+          ]
+        }
+      ];
+  
+        this.deleteConfirmQuestions = [
+        {
+          type: "confirm",
+          name: "confirmation",
+          message: "are you sure you want to delete this contact?"
+        }
+      ];
+    }
+
       addContact(name, phone, email){
         return Contact.create({name, phone, email})
       }
+      getContacts(){
+        return Contact.findAll()
+      }
+      iterativeSearch(contacts, target){
+        for(let contact of contacts){
+            if(contact.name.toLowerCase() === target.toLowerCase()){
+              return contact;
+            }
+          }
+          return null;
+      }
 
+      binarySearch(contacts, target){
+        let min = 0;
+        let max = contacts.length - 1;
+        let mid;
+  
+        while(min <= max) {
+  
+  // #1
+          mid = Math.floor((min + max) / 2);
+          let currentContact = contacts[mid];
+  
+  // #2
+          if(currentContact.name > target){ //target is before element at mid, eliminate upper bound
+            max = mid - 1;
+          } else if(currentContact.name < target){ //target is after element at mid, eliminate lower bound
+            min = mid + 1;
+          } else { //element found, return it
+            return contacts[mid];
+          }
+        }
+  
+        return null;
+      }
+
+      search(name){
+        return Contact.findOne({
+          where: {name}
+        });
+      }
+
+      delete(id){
+        return Contact.destroy({
+          where: {id}
+        })
+      }
     }
--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -3,7 +3,7 @@ const ContactController = require("./ContactController");
 
 module.exports = class MenuController {
     constructor(){
-      
+
       this.mainMenuQuestions = [
    {
      type: "list",
@@ -11,7 +11,8 @@ module.exports = class MenuController {
      message: "Please choose from an option below: ",
      choices: [
        "Add new contact",
-       "Get date and time",
+       "View all contacts",
+       "Search for a contact",
        "Exit"
      ]
    }
@@ -26,12 +27,12 @@ module.exports = class MenuController {
         case "Add new contact":
           this.addContact();
           break;
-        case "Get date and time":
-           this.getDate();
-           break;
-        case 'Remind Me':
-           this.remindMe();
-           break;
+        case "View all contacts":
+          this.getContacts();
+          break;
+        case "Search for a contact":
+          this.search();
+          break;
         case "Exit":
           this.exit();
         default:
@@ -51,7 +52,7 @@ module.exports = class MenuController {
     addContact(){
      this.clear();
      inquirer.prompt(this.book.addContactQuestions).then((answers) => {
-      this.book.addContact(answers.name, answers.phone).then((contact) => {
+      this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
         console.log("Contact added successfully!");
         this.main();
       }).catch((err) => {
@@ -60,26 +61,100 @@ module.exports = class MenuController {
       });
     });
    }
- 
-   getDate(){
-    const now = new Date();
+   
+   getContacts(){
     this.clear();
-    console.log(now.toLocaleString());
-    this.main();
+
+    this.book.getContacts().then((contacts) => {
+      for (let contact of contacts) {
+        console.log(`
+        name: ${contact.name}
+        phone number: ${contact.phone}
+        email: ${contact.email}
+        ---------------`
+        );
+      }
+      this.main();
+    }).catch((err) => {
+      console.log(err);
+      this.main();
+    });
   }
- 
+
+  search(){
+    inquirer.prompt(this.book.searchQuestions)
+    .then((target) => {
+     this.book.search(target.name)
+     .then((contact) => {
+        if(contact === null){
+          this.clear();
+          console.log("contact not found");
+          this.search();
+        } else {
+          this.showContact(contact);
+       }
+
+      });
+   })
+   .catch((err) => {
+     console.log(err);
+     this.main();
+   });
+  }
+
+  showContact(contact){
+    this._printContact(contact);
+    inquirer.prompt(this.book.showContactQuestions)
+    .then((answer) => {
+      switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+  }
+
+  _printContact(contact){
+    console.log(`
+      name: ${contact.name}
+      phone number: ${contact.phone}
+      email: ${contact.email}
+      ---------------`
+    );
+  }
+   
+  delete(contact){
+    inquirer.prompt(this.book.deleteConfirmQuestions)
+    .then((answer) => {
+      if(answer.confirmation){
+        this.book.delete(contact.id);
+        console.log("contact deleted!");
+        this.main();
+      } else {
+        console.log("contact not deleted");
+        this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.main();
+    });
+  }
+
    exit(){
      console.log("Thanks for using AddressBloc!");
      process.exit();
    }
-
-  
-
-  remindMe() {
-    console.log('Learning is a life-long pursuit');
-    this.main();
-    return 'Learning is a life-long pursuit';
-  }
 
  }
  
